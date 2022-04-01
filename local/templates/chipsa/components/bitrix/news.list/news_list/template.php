e6ad03a7f29dcd6133b111d1bc6c29789ece4025
jaguarcion <amokrecov@mail.ru>
2022-04-01 17:54:04 +0700

    some changes

--- local/templates/chipsa/components/bitrix/news.list/news_list/template.php
+++ local/templates/chipsa/components/bitrix/news.list/news_list/template.php
@@ -24,45 +24,55 @@ $this->setFrameMode(true);
 	?>
 
 		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-            <img
-                    class="preview_picture"
-                    border="0"
-                    src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-                    width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-                    height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-                    alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-                    title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-                    style="float:left"
-                    />
+            <div class="news-image">
+                <img
+                        class="preview_picture"
+                        border="0"
+                        src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                        width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                        height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                        alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                        title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                        style="float:left"
+                        />
+            </div>
 		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a class="news-name" href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b ><?echo $arItem["NAME"]?></b></a>
-			<?else:?>
-				<b class="news-name"><?echo $arItem["NAME"]?></b>
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small>
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small>
-		<?endforeach;?>
+        <div class="news-block-content">
+            <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+                <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                    <a class="news-name" href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></a>
+                <?else:?>
+                    <?echo $arItem["NAME"]?>
+                <?endif;?>
+            <?endif;?>
+            <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+                <div class="news-short-text">
+                    <?echo $arItem["PREVIEW_TEXT"];?>
+                </div>
+            <?endif;?>
+            <?foreach($arItem["FIELDS"] as $code=>$value):?>
+                <small>
+                <?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
+                </small>
+            <?endforeach;?>
+            <div class="news-date">
+                <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+                    <span class="news-date-time">Дата публикации: <?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+                <?endif?>
+            </div>
+            <div class="news-author">
+                <?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+                    <small>
+                    <?=$arProperty["NAME"]?>:&nbsp;
+                    <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+                        <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+                    <?else:?>
+                        <?=$arProperty["DISPLAY_VALUE"];?>
+                    <?endif?>
+                    </small>
+                <?endforeach;?>
+            </div>
+        </div>
 </div>
 <?endforeach;?>
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
