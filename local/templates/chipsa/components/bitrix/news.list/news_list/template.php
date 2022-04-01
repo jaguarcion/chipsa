719a7e0514fe9b3d6aa02c64fec393da04cb9216
jaguarcion <amokrecov@mail.ru>
2022-04-01 17:45:56 +0700

    remove a tag from image

--- local/templates/chipsa/components/bitrix/news.list/news_list/template.php
+++ local/templates/chipsa/components/bitrix/news.list/news_list/template.php
@@ -24,29 +24,16 @@ $this->setFrameMode(true);
 	?>
 
 		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
+            <img
+                    class="preview_picture"
+                    border="0"
+                    src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                    width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                    height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                    alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                    title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    style="float:left"
+                    />
 		<?endif?>
 		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
 			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
